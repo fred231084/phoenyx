bed40483bafba227c31e222dbcf3e7103a811f92
Luke Carr <luke@omniointeractive.com>
2020-02-20 00:03:06 +0000

    Initial Maven project structure setup

---
+++ plugin/src/main/java/com/omniointeractive/phoenyx/PhoenyxPlugin.java
@@ -0,0 +1,9 @@
+package com.omniointeractive.phoenyx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main plugin class for Phoenyx. This class acts as the main entry point for Spigot into the Phoenyx.
+ */
+public class PhoenyxPlugin extends JavaPlugin {
+}
