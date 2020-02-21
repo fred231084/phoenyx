28027634418905ee455acf45d793769ccb2928da
Luke Carr <luke@omniointeractive.com>
2020-02-21 03:32:30 +0000

    Fixed typo in javadocs

--- api/src/main/java/com/omniointeractive/phoenyx/api/item/interfaces/Placeable.java
+++ api/src/main/java/com/omniointeractive/phoenyx/api/item/interfaces/Placeable.java
@@ -1,7 +1,7 @@
 package com.omniointeractive.phoenyx.api.item.interfaces;
 
 /**
- * Interface used to indicate an {@link com.omniointeractive.phoenyx.api.item.Item} that can be placed in the world.
+ * Interface used to indicate that an {@link com.omniointeractive.phoenyx.api.item.Item} can be placed in the world.
  * By default any custom {@link com.omniointeractive.phoenyx.api.item.Item} cannot be placed in the world, overriding
  * vanilla behavior.
  */
